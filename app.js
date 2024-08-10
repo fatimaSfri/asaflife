1c08d0b59eaf33ef08ba81bb57527454f3bb9d8d
fatima <sfateme262@gmail.com>
2024-08-10 09:17:08 +0330

    v-2 dashbord

--- app.js
+++ app.js
@@ -37,10 +37,6 @@
 
 // }
 
-
-
-
-
 const toggleBtn =document.querySelector('.toggle-button')
 const dropdown=document.querySelector(".dropdown-menu")
 
@@ -54,7 +50,7 @@ let imgSlider =document.querySelector('#parent')
 let childElem=document.querySelector('#childfrist')
 let btnNext=document.querySelector('#next')
 let btnPrev=document.querySelector('#prev')
-
+let dot =document.querySelectorAll(".dot")
 let a_img=['./img/bg/03.jpg','./img/bg/04.jpg','./img/bg/02.jpg','./img/bg/05.jpg',]
 let index = 0;
 
@@ -64,19 +60,15 @@ setInterval(() => changeSlider(-1) ,10000);
 
 const changeSlider = (step) => 
 {
+    
     index = (index + step + a_img.length) % a_img.length;
-    imgSlider.style.backgroundImage = `url(${a_img[index]})`;
+    imgSlider.style.backgroundImage = `url(${a_img[index]})`;   
 }
 
- 
-btnNext.addEventListener('click',function(){
-
-    console.log('hiii')
-})
- 
 
 
 
-console.log(btnNext)
+
+
 
 
