1f2a196f0a6707d4066b220fce07610db694fe85
safari <sfateme262@gmail.com>
2024-08-29 11:36:04 +0330

    add slider

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,42 @@
 const toggleBtn =document.querySelector('.toggle-button')
-const dropdown=document.querySelector(".dropdown-menu")
-let s=0
+ const dropdown=document.querySelector(".dropdown-menu")
+ let s=0
 
-window.addEventListener('scroll', function(){
+ window.addEventListener('scroll', function(){
 
 s = dropdown.scrollTop
 console.log(s)
-
 })
-
 
 toggleBtn.addEventListener('click',() =>
 {
-  
  dropdown.classList.toggle('top-[4.1rem]')
-
-
 })
-
-
 
 let imgSlider =document.querySelector('#parent')
 let dot =document.querySelectorAll(".dot")
-// let a_img=['./img/bg/02.jpg', './img/bg/03.jpg','./img/bg/04.jpg','./img/bg/05.jpg','./img/bg/06.jpg']
+
+let a_img=['bg-small','bg-medium','bg-large'];
+
 let index = 0;
 
 setInterval(() => changeSlider(-1) ,10000);
 
+ const changeSlider = (step,inx=null) => 
+ {
 
-const changeSlider = (step,inx=null) => 
-{
-    
     if(inx != null){
       index=inx
      }
 
     index = (index + step + a_img.length) % a_img.length;
     imgSlider.style.backgroundImage = `url(${a_img[index]})`; 
+     
+     
   
-
-}
+ }
 
 window.changeSlider = changeSlider;
-
 
 const dots = document.getElementsByClassName('dot');
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
     extend: {
       fontFamily: {
         asaf: ["asaf", "Asaf"],
-      
       },
+      backgroundImage:{
+       "back_small_01":"url('../img/bg/S.jpg')",
+       "back_medium_01":"url('../img/bg/M.jpg')",
+       "back_large_01":"url('../img/bg/L.jpg')",
+       "back_small_02":"url('../img/bg/S2.jpg')",
+       "back_medium_02":"url('../img/bg/M2.jpg')",
+       "back_large_02":"url('../img/bg/L2.jpg')",
+       "back_small_03":"url('../img/bg/S3.jpg')",
+       "back_medium_03":"url('../img/bg/M3.jpg')",
+       "back_large_03":"url('../img/bg/L3.jpg')",
+
+
+      }
   },
   plugins: [],
 }
